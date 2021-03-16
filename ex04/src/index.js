85c18234436c87fafb84ecb7cae690405e14123d
Hamzabyte96 <brcaninovichamza@yahoo.com>
2021-03-16 19:53:01 +0100

    izmjene ex04

---
+++ ex04/src/index.js
@@ -0,0 +1,14 @@
+function compareDifferentValues(m, n) {
+    if(m === n) {
+        return "Equal";
+    }
+    return "NotEqual";
+
+}
+
+console.log(compareDifferentValues(8, "8"));
+console.log(compareDifferentValues("8", 8));
+console.log(compareDifferentValues('8', 8));
+console.log(compareDifferentValues("8", "8"));
+console.log(compareDifferentValues(8, 8));
+module.exports = compareDifferentValues;
