46fa72c4b54a3c87a3b66ddc77e00c8088d8c0aa
Hamzabyte96 <brcaninovichamza@yahoo.com>
2021-03-17 00:19:40 +0100

    izmjene ex13

---
+++ ex13/src/index.js
@@ -0,0 +1,9 @@
+function inverseWhile() {
+    var fiveNumbers = 0;
+    while(fiveNumbers < 5) {
+        console.log("5,4,3,2,1,0")
+        fiveNumbers+=5
+    }
+}
+console.log(inverseWhile());
+module.exports = inverseWhile;
